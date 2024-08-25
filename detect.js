3a5091e9485b93bf31686f22b95e384437c1e25d
Игорь <93474580+lgor360@users.noreply.github.com>
2024-08-25 15:43:14 +0300

    Update detect.js

--- detect.js
+++ detect.js
@@ -1,1 +1,1 @@
-if (navigator.platform === "New Nintendo 3DS") alert('у меня грустная новость...');
+if (navigator.platform === "New Nintendo 3DS") alert('у меня грустная новость... этот сайт не поддерживает консоли семейства 3DS... нажмите ок чтобы закрыть сайт :(');
