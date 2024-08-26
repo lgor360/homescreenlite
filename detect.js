5c894d6108cceff085a94561e8170b7056d4ec6d
Игорь <93474580+lgor360@users.noreply.github.com>
2024-08-26 17:39:32 +0300

    Create detect.js

---
+++ detect.js
@@ -0,0 +1,11 @@
+if (navigator.platform === "New Nintendo 3DS") {
+  alert("у меня один вопросик...");
+  alert("НАФИГА ТЕБЕ ДРУГАЯ ДОМАШНЯЯ СТРАНИЦА НА 3DS?");
+  location.href = "https://lgor360.github.io/3dswebsite/home.html"
+} else if (navigator.platform === "Nintendo 3DS") {
+  alert("у меня один вопросик...");
+  alert("НАФИГА ТЕБЕ ДРУГАЯ ДОМАШНЯЯ СТРАНИЦА НА 3DS?");
+  location.href = "https://lgor360.github.io/3dswebsite/home.html"
+} else {
+  console.log('у тебя не консоль семейтсва 3DS :D');
+}
